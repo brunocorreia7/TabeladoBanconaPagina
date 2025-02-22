b3d684b75a46d90a377445d58eb5d75f92a9bb0b
Bruno Correia <correiadesigner7@gmail.com>
2025-02-21 23:58:48 -0300

    processo de criaçao do cadastro

--- funcoes.php
+++ funcoes.php
@@ -1,15 +1,11 @@
 <?php
-if (!function_exists('buscarDadosTabela')) {
-    function buscarDadosTabela($conn) {
-        $sql = 'SELECT * FROM tabela';
-        return mysqli_query($conn, $sql);
-    }
+function buscarDadosTabela($conn) {
+    $sql = 'SELECT * FROM tabela';
+    return mysqli_query($conn, $sql);
 }
 
-if (!function_exists('cadastrarUsuario')) {
-    function cadastrarUsuario($conn, $nome, $email, $estado, $profissao) {
-        $sql = "INSERT INTO tabela (nome, email, estado, profissao) VALUES ('$nome', '$email', '$estado', '$profissao')";
-        return mysqli_query($conn, $sql);
-    }
+function cadastrarUsuario($conn, $nome, $email, $estado, $profissao) {
+    $sql = "INSERT INTO tabela (nome, email, estado, profissao) VALUES ('$nome', '$email', '$estado', '$profissao')";
+    return mysqli_query($conn, $sql);
 }
 ?>
